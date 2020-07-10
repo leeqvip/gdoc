a6c416383fc30aac97e185c045e1dd9866911b4c
Foufou <techlee@qq.com>
2020-07-10 18:23:58 +0800

    Update router.go

--- router.go
+++ router.go
@@ -4,8 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func UseInGin(g *gin.Engine)  {
-	g.GET("/docs/*name", func(context *gin.Context) {
+func UseInGin(r gin.IRouter)  {
+	r.GET("/docs/*name", func(context *gin.Context) {
 		body := Handle(context.Param("name"))
 		context.Header("Content-Type", "text/html; charset=utf-8")
 		context.String(200, string(body))
